496323e5979fd703db6e233e02b12cdd02800d4a
lulu <1479331506@qq.com>
2022-09-22 10:18:31 +0800

    2022.9.22 1.0

--- internal/model/createlog.go
+++ internal/model/createlog.go
@@ -12,7 +12,8 @@ type Log struct {
 	GameId  int          `json:"game_id" form:"game_id"` // 游戏id
 	Tab     string       `json:"tab" form:"tab"`         // 游戏的不同玩法标志
 	Name    string       `json:"name" form:"name"`       // 游戏名
-	Version int          `json:"version" form:"version"` // 版本号(代表当前收到的content切片数量)
+	Version int          `json:"version" form:"version"` // 版本号
+	Count   int          `json:"count" form:"count"`     // 代表当前收到的content切片数量
 	Logs    []LogContent // 更新 结构体类型 切片
 }
 
@@ -40,6 +41,7 @@ func CreateLog(log *Log, logcontents []LogContent) {
 
 	if logcon.Id != 0 { // 若是存在 则执行覆盖操作
 		logcon.Version = log.Version
+		logcon.Count = log.Count
 		db.Save(&logcon)
 		fmt.Println("覆盖游戏玩法具体信息...")
 
@@ -48,8 +50,6 @@ func CreateLog(log *Log, logcontents []LogContent) {
 
 		var count int
 		db.Table("log_contents").Where("log_id=?", id).Count(&count) // 获取游戏的tab下有几条记录
-
-		if len(logcontents) >= count { //若是更新的数量大于数据库中的记录数，正常更新
 			for _, logcontent := range logcontents { // 遍历收到的切片
 				contentcon := LogContent{}                                                           // 准备一个LogContent容器，装查询到的数据
 				logcontent.LogId = id                                                                // 将对应的logcontents切片中的logid全部赋值上id值以形成联系
@@ -63,14 +63,6 @@ func CreateLog(log *Log, logcontents []LogContent) {
 					fmt.Println("编号为", id, "的tab，更新时间戳为", logcontent.UpdateTime, "的玩法更新已经新建")
 				}
 			}
-		} else { // 若是更新的数量小于数据库中的数据数量，删除此tab的更新记录重新插入（保证数据库中没有垃圾数据）
-			db.Delete(LogContent{}, "log_id = ?", id)
-			for _, logcontent := range logcontents {
-				logcontent.LogId = id
-				db.Create(&logcontent)
-				fmt.Println("编号为", id, "的tab，更新时间戳为", logcontent.UpdateTime, "的玩法更新已经新建")
-			}
-		}
 	} else { // 不存在则直接插入
 		db.Create(&log)
 		id := logcon.Id
--- router/router.go
+++ router/router.go
@@ -9,7 +9,9 @@ func Start(){
 	r := gin.Default()  // 返回默认的路由引擎
 
 	m := r.Group("/manage") 			// 后台管理系统的接口
-	m.POST("/game",handler.GetLog)  	// 新建或修改游戏以及日志（方式是存在则覆盖，不存在则创建）
+	m.POST("/game",handler.GetLog)  	// 新建或修改游戏玩法以及日志（方式是存在则覆盖，不存在则创建）
+	// m.DELETE("/game/content",handler.DelLogContent) // 删除指定的日志详情
+	// m.DELETE("/game/log")			// 删除游戏玩法（包括该玩法下的所有日志详情）
 
 	r.Run(":8080") //监听8080端口
 }
