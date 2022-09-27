cc2d6fb6fbddc428facb59bdcc70fbdd127f039c
lulu <1479331506@qq.com>
2022-09-27 17:48:43 +0800

    2022.9.27 2.0

--- internal/model/createlog.go
+++ internal/model/createlog.go
@@ -9,6 +9,7 @@ import (
 type Log struct {
 	Id      string       `json:"id"`      // 数据库中的id 一个id对应一个游戏的一个tag（前端使用uuid生成传入）
 	GameId  int          `json:"game_id"` // 游戏id									（自己规定）
+	Name    string                        // 游戏名称
 	Tab     string       `json:"tab"`     // 游戏的不同玩法标志
 	Version int          `json:"version"` // 版本号
 	Logs    []LogContent `json:"logs"`    // 更新 结构体类型 切片
